8016b87e2d3a946f97e18ba894f2a77d4bfbaa7e
Podshibyakin Maksim <maksim_038@mail.ru>
2022-06-29 00:51:27 +0300

    исправить JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,42 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseElement = popupElement.querySelector('.popup__button-close');
 const popupOpenElement = document.querySelector('.profile__editor');
-const popupSaveform = popupElement.querySelector('.popup__button-save')
+const popupInputNameElement = popupElement.querySelector('.popup__input_name_valve');
+const popupInputJobElement = popupElement.querySelector('.popup__input_job_valve');
+const profileName = document.querySelector('.profile__name');
+const pofileCaption = document.querySelector('.profile__caption')
+let formElement = document.querySelector('.popup__form');
+
+
+
 
 
 const openPopup = function() {
-    document.querySelector('.popup__form-name').value = document.querySelector('.profile__name').textContent;
-    document.querySelector('.popup__form-job').value = document.querySelector('.profile__caption').textContent;
+    popupInputNameElement.value = profileName.textContent;
+    popupInputJobElement.value = pofileCaption.textContent;
     popupElement.classList.add('popup_opened');
 
-    console.log('openPopup');
+
 }
 const closePopup = function() {
     popupElement.classList.remove('popup_opened');
-    console.log('closePopup')
+
 }
-const closePopupByOverlay = function(event) {
-    if (event.target !== event.currentTarget) {
-        return
-    }
-    closePopup();
-}
-
-popupOpenElement.addEventListener('click', openPopup)
-popupCloseElement.addEventListener('click', closePopup)
-popupElement.addEventListener('click', closePopupByOverlay)
-
-
-
-let formElement = document.querySelector('.popup__forms');
-
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let nameInput = formElement.querySelector('.popup__form-name').value;
-    let jobInput = formElement.querySelector('.popup__form-job').value;
+    let nameInput = popupInputNameElement.value;
+    let jobInput = popupInputJobElement.value;
 
 
-    let nameProfile = document.querySelector('.profile__name');
-    let jobProfile = document.querySelector('.profile__caption');
+    let nameProfile = profileName;
+    let jobProfile = pofileCaption;
+
 
     nameProfile.textContent = nameInput;
     jobProfile.textContent = jobInput;
-
+    closePopup()
 }
 formElement.addEventListener('submit', formSubmitHandler);
-popupSaveform.addEventListener('click', formSubmitHandler);
-popupSaveform.addEventListener('click', closePopup);
+popupOpenElement.addEventListener('click', openPopup)
+popupCloseElement.addEventListener('click', closePopup)
