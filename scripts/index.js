3b8db4d5becb156ed4026651eb8a41caabfca4e8
Podshibyakin Maksim <maksim_038@mail.ru>
2022-06-29 22:16:37 +0300

    после 2 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,33 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseElement = popupElement.querySelector('.popup__button-close');
 const popupOpenElement = document.querySelector('.profile__editor');
-const popupInputNameElement = popupElement.querySelector('.popup__input_name_valve');
-const popupInputJobElement = popupElement.querySelector('.popup__input_job_valve');
+const popupInputNameElement = popupElement.querySelector('.popup__input_valve_name');
+const popupInputJobElement = popupElement.querySelector('.popup__input_valve_job');
 const profileName = document.querySelector('.profile__name');
 const pofileCaption = document.querySelector('.profile__caption')
 let formElement = document.querySelector('.popup__form');
-
-
-
-
 
 const openPopup = function() {
     popupInputNameElement.value = profileName.textContent;
     popupInputJobElement.value = pofileCaption.textContent;
     popupElement.classList.add('popup_opened');
-
-
 }
+
 const closePopup = function() {
     popupElement.classList.remove('popup_opened');
-
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     let nameInput = popupInputNameElement.value;
     let jobInput = popupInputJobElement.value;
-
-
     let nameProfile = profileName;
     let jobProfile = pofileCaption;
-
-
     nameProfile.textContent = nameInput;
     jobProfile.textContent = jobInput;
     closePopup()
 }
+
 formElement.addEventListener('submit', formSubmitHandler);
 popupOpenElement.addEventListener('click', openPopup)
 popupCloseElement.addEventListener('click', closePopup)
