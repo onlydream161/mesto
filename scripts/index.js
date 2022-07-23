4a785d38e3803d9fed5db436fca87f6ca8489e4d
Podshibyakin Maksim <maksim_038@mail.ru>
2022-07-24 00:43:11 +0300

    отделить массив

--- scripts/index.js
+++ scripts/index.js
@@ -23,33 +23,6 @@ const itemCard = addCard.querySelector('.places__card');
 const popupButtonSave = popupProfileEdit.querySelector('.popup__button-save');
 const popupAddCardButtonSave = popupAddCard.querySelector('.popup__button-save')
 
-console.log(document.getElementsByName('namejob'))
-const initialCards = [{
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
 // создание карточки
 function createCard(name, link) {
     const cardElement = itemCard.cloneNode(true);
